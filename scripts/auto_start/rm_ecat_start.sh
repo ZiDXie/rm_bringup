25fb01d4d9c06276374cbb99036c50615d08c1fc
ZiDXie <shengxie47@gmail.com>
2025-09-08 18:09:23 +0800

    Update service dependencies and streamline startup scripts.

--- scripts/auto_start/rm_ecat_start.sh
+++ scripts/auto_start/rm_ecat_start.sh
@@ -3,9 +3,4 @@ source /opt/ros/noetic/setup.bash
 source /home/dynamicx/rm_ws/devel/setup.bash
 source /home/dynamicx/mpc_ws/devel/setup.bash
 source /home/dynamicx/environment.sh
-if [[ $HAS_SWITCH == has ]]; then
-  export ROS_IP=192.168.100.2
-else
-  export ROS_IP=127.0.0.1
-fi
 mon launch --disable-ui --log=/tmp/hw.log rm_bringup $LAUNCH.launch
--- scripts/auto_start/start_master.sh
+++ scripts/auto_start/start_master.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.bash
+sleep 2
 source ~/environment.sh
 if [[ $HAS_SWITCH == has ]]; then
   export ROS_IP=192.168.100.2
